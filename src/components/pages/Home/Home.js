2ab49721a4059f16a3b7ab9f095477297dc44059
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-10 20:19:15 +0200

    render Tables in Home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import { getAllTables } from "../../../redux/tablesRedux";
+import Tables from "../../features/Tables/Tables";
 
 const Home = () => {
   const tables = useSelector(getAllTables);
   return (
     <div>
       <h2>All tables</h2>
+      <Tables tables={tables} />
     </div>
   );
 };
