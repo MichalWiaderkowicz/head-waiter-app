18409b91d5db38ab764771866d8bcd4657e70c3d
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-10 18:41:45 +0200

    add cslx and change code in Footer.js

--- src/components/pages/views/Footer/Fooer.js
+++ src/components/pages/views/Footer/Fooer.js
@@ -1,5 +1,12 @@
+import { clsx } from "clsx";
+import React from "react";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <p className={clsx("text-center", "text-muted", "mt-5", "mb-5")}>
+      Copyright © head-waiter-app 2024
+    </p>
+  );
 };
 
 export default Footer;
