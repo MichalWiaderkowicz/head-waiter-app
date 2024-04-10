21408624f7c6996616a89d5a2afcf76a0fe45cd5
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-10 19:57:13 +0200

    add features/Tables/Tables.js

---
+++ src/components/features/Tables/Tables.js
@@ -0,0 +1,34 @@
+import { Row, Card, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Tables = ({ tables }) => {
+  return (
+    <Row>
+      {tables.map((table) => (
+        <Card key={table.id} style={{ width: "100%" }}>
+          <Card.Body className="d-flex align-items-center">
+            <Card.Title>
+              <strong>Table {table.id}</strong>
+            </Card.Title>
+            <Card.Text>
+              <span>
+                <strong>Status: </strong>
+                {table.status}
+              </span>
+            </Card.Text>
+            <Button
+              className="text-end"
+              variant="primary"
+              as={NavLink}
+              to={"/table/" + table.id}
+            >
+              Show more
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </Row>
+  );
+};
+
+export default Tables;
