b370f9f133dc73c3e8c0016635f6b3f19a16b00d
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-10 18:11:32 +0200

    add Routes to Pages: Home, Table and NotFound in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home.js";
+import Table from "./components/pages/Table/Table.js";
+import NotFound from "./components/pages/NotFound/NtFound.js";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main>
+      <h1>Hello world</h1>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table/:tableId" element={<Table />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </main>
   );
 }
 
