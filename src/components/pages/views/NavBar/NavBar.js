746f95ec460cdcb11e707e73decf55d5eb5690f4
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-10 18:36:33 +0200

    change code in NavBar.js

--- src/components/pages/views/NavBar/NavBar.js
+++ src/components/pages/views/NavBar/NavBar.js
@@ -1,5 +1,28 @@
-const NavBar = () => {
-  return <h1>Navigation</h1>;
-};
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
 
-export default NavBar;
+export const NavBar = () => {
+  const location = useLocation();
+  return (
+    <Navbar
+      expand="lg"
+      bg="primary"
+      variant="dark"
+      className="mt-4 mb-4 rounded"
+    >
+      <Container>
+        <Navbar.Brand href="/">head-waiter.app</Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Link
+            as={NavLink}
+            to="/"
+            className={location.pathname === "/" ? "active" : ""}
+          >
+            Home
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
