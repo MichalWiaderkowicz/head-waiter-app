e27317dd8a1f3e034d298cc924ea2b997a652d45
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-11 11:26:03 +0200

    change navBar.barnd href

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home.js";
 import Table from "./components/pages/Table/Table.js";
-import NotFound from "./components/pages/NotFound/NtFound.js";
+import NotFound from "./components/pages/NotFound/NotFound.js";
 import Footer from "./components/pages/views/Footer/Footer.js";
 import Header from "./components/pages/views/Header/Header.js";
 import { useDispatch } from "react-redux";
